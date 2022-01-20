edf550a650f3b7001bea571601d895ebabbc178f
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-01-20 16:44:08 +0100

    aggiunta secondo metodo

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+//METODO COMPATTO
+/*
 //chiedi età
-let eta = prompt('inserisci età');
-console.log(eta);
+let eta = prompt('inserisci età')
 
 //chiedo km
 let km = prompt('quanti km devi fare');
-console.log(km);
 
-
+//operazione
 if (eta < 18) {
     document.getElementById('biglietto').innerHTML = 'Il costo del tuo biglietto è ' + ((km * 0.21) - ((km * 0.21) / 100) * 20).toFixed(2);
 } else if (eta >= 65) {
@@ -14,3 +14,24 @@ if (eta < 18) {
 } else {
     document.getElementById('biglietto').innerHTML = 'Il costo del tuo biglietto è ' + (km * 0.21).toFixed(2);
 };
+*/
+
+
+
+//METODO ESTESO
+//chiedi età
+let eta = prompt('inserisci età');
+
+//chiedo km
+let km = prompt('quanti km devi fare');
+
+//operazione
+if (eta < 18) {
+    var risultato = ((km * 0.21) - ((km * 0.21) / 100) * 20);
+} else if (eta >= 65) {
+    var risultato = ((km * 0.21) - ((km * 0.21) / 100) * 40);
+} else {
+    var risultato = (km * 0.21);
+};
+
+document.getElementById('biglietto').innerHTML = 'Il costo del tuo biglietto è ' + risultato.toFixed(2);
